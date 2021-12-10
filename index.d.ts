b99b072ec3c13a765235c304bab14aecd6d1c700
Noah Greff <noahgreff@gmail.com>
2021-12-10 13:00:01 +0100

    added comments to types

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,83 @@
+/**
+ * Available log types.
+ */
 type LogType = 'debug' | 'warn' | 'error' | 'info' | 'important' | 'success' | 'data' | 'label' | 'chuckle'
 
+/**
+ * Logging verbose types.
+ * `debug` => logs everything
+ * `warning` => logs everything EXCEPT (`debug` and `data` logs)
+ * `errors` => display everything EXCEPT (`debug`, `data` and `warning` logs)
+ */
 export type Verbose = 'debug' | 'warning' | 'error'
 
+/**
+ * Default logger config options.
+ */
 export type LoggerOptions = {
-    isDevelopment?: boolean
+	/**
+	 * `isDevelopment` (default: `process.env.NODE_ENV`)
+	 * Dictates whether `error` logs with errors displays the stacktrace.
+	 * @type {boolean}
+	 */
+	isDevelopment?: boolean
+
+	/**
+	 * `verbose` (default: `debug`)
+	 * Dictates what the logger can display
+	 * @type {Verbose}
+	 */
 	verbose?: Verbose
+
+	/**
+	 * `width` (default: `80`)
+	 * Defines the horizontal boundaries of the console.
+	 * @type {number}
+	 */
 	width?: number
+
+	/**
+	 * `handleUncaughtException` (default `false`)
+	 * Whether the logger handles `uncaughtException` events.
+	 * @type {boolean}
+	 */
 	handleUncaughtException?: boolean
+
+	/**
+	 * `handleUnhandledRejection` (default `false`)
+	 * Whether the logger handles `unhandledRejection` events.
+	 * @type {boolean}
+	 */
 	handleUnhandledRejection?: boolean
+
+	/**
+	 * `handleWarning` (default `false`)
+	 * Whether the logger handles `warning` events.
+	 * @type {boolean}
+	 */
 	handleWarning?: boolean
 }
 
+/**
+ * Creates an instance of a Logger with given options.
+ * @param {LoggerOptions?} config Logger config options.
+ * @returns {Logger}
+ */
 export function createLogger(options?: LoggerOptions): Logger.Logger
 
 export namespace Logger {
+	/**
+	 * Log type object for the logger to process via `processLog`.
+	 */
 	export type Log = {
 		info: any
 		label: string
 		style: Style.LogStyle
 	}
 
+	/**
+	 * Logger object. All available log types.
+	 */
 	export interface Logger {
 		debug(...info: any): void
 		warn(...info: any): void
@@ -40,22 +98,72 @@ export namespace Logger {
 }
 
 export namespace Style {
+	/**
+	 * Styling object for the logger to process via `processLog`.
+	 */
 	export class LogStyle {
 		constructor(fg: string, bg: string)
 		fg(line: string): string
 		bg(line: string): string
 	}
 
+	/**
+	 * Wraps input with the highlight escape sequence.
+	 * @param {string} line input.
+	 */
 	export function bold(line: string): string
 
+	/**
+	 * Special style for `debug` logs.
+	 * @type {LogStyle}
+	 */
 	export const Debug: LogStyle
+
+	/**
+	 * Spectial style for `data` logs.
+	 * @type {LogStyle}
+	 */
 	export const Data: LogStyle
 
+	/**
+	 * `Red` log styling.
+	 * @type {LogStyle}
+	 */
 	export const Red: LogStyle
+
+	/**
+	 * `Green` log styling.
+	 * @type {LogStyle}
+	 */
 	export const Green: LogStyle
+
+	/**
+	 * `Yellow` log styling.
+	 * @type {LogStyle}
+	 */
 	export const Yellow: LogStyle
+
+	/**
+	 * `Blue` log styling.
+	 * @type {LogStyle}
+	 */
 	export const Blue: LogStyle
+
+	/**
+	 * `Magenta` log styling.
+	 * @type {LogStyle}
+	 */
 	export const Magenta: LogStyle
+
+	/**
+	 * `Cyan` log styling.
+	 * @type {LogStyle}
+	 */
 	export const Cyan: LogStyle
+
+	/**
+	 * `White` log styling.
+	 * @type {LogStyle}
+	 */
 	export const White: LogStyle
 }
